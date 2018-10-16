96d9ba002fcdf4e73abd3fdb01198d496a0d5896
Harrisson Cruz <harrisson.cruz@drlavatudo.com>
2018-10-15 21:03:19 -0300

    Inicial commit

---
+++ Trabalho IHC Quiz/Questao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_IHC_Quiz
+{
+    class Questao
+    {
+
+        public string Pergunta { get; set; }
+
+        public string Opcao1 { get; set; }
+        public string Opcao2 { get; set; }
+        public string Opcao3 { get; set; }
+        public string Opcao4 { get; set; }
+
+        public string Resposta { get; set; }
+        public string Dica { get; set; }
+
+    }
+}
